1f49d409e17962449c3a3d95fc60050b3837afb2
ra54283suzuki <ra54283suzuki@gmail.com>
2022-05-15 23:26:00 +0900

    GET request (character)

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(req, res) {
+  res.send("<h1>こんにちはの見出し</h1>");
+});
+
 app.listen(5000, function() {
   console.log("Listening on localhost port 5000");
 });
