8f012fe7f7ebbf880dd7d7ab0b4b33509ddb3022
ra54283suzuki <ra54283suzuki@gmail.com>
2022-05-17 23:09:03 +0900

    POST request

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function(req,res) {
+  console.log("reqの中身", req.body);
 });
 
 app.listen(5000, function() {
